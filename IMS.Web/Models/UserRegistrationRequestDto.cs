becb25ec180f31ac94d5a726add29de6f716e84a
Htyagi2003 <tyagih893@gmail.com>
2024-08-04 13:34:02 +0530

    Login And Registration UI Completed

--- IMS.Web/Models/UserRegistrationRequestDto.cs
+++ IMS.Web/Models/UserRegistrationRequestDto.cs
@@ -1,11 +1,18 @@
-﻿namespace IMS.Web.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IMS.Web.Models
 {
     public class UserRegistrationRequestDto
     {
+        [Required]
         public string Email { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string PhoneNumber { get; set; }
+   
         public string Role {  get; set; }
+        [Required]
         public string Password { get; set; }
     }
 }
